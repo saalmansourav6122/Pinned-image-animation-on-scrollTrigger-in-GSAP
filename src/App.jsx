4b32772ee75d2a8b86e26e913a2d657a0d922ea9
saalmansourav6122 <saalmansourav111@gmail.com>
2025-03-26 21:35:12 +0600

    added array

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,27 @@ import "./App.css";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap/all";
 function App() {
+  const array = [
+    {
+      headLine: "We can do for You",
+      info: [
+        {
+          title: "One",
+          description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet commodi vel velit libero non deserunt veniam dolores quae cupiditate tenetur?"
+        },
+        {
+          title: "Two",
+          description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet commodi vel velit libero non deserunt veniam dolores quae cupiditate tenetur?"
+        },
+        {
+          title: "Three",
+          description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet commodi vel velit libero non deserunt veniam dolores quae cupiditate tenetur?"
+        }
+      ],
+      image: ["https://picsum.photos/200/300", "https://picsum.photos/id/237/200/300", "https://picsum.photos/seed/picsum/200/300", "https://picsum.photos/200/300?grayscale"]
+    }
+  ];
+  
   useGSAP(() => {
     gsap.to(".box", {
       duration: 1,
@@ -11,10 +32,9 @@ function App() {
   });
   return (
     <>
-      <div className="w-[250px] h-[250px] bg-amber-300"></div>
-      <div className="w-[250px] h-[250px] bg-amber-300"></div>
-      <div className="w-[250px] h-[250px] bg-amber-300"></div>
-      <div className="w-[250px] h-[250px] bg-amber-300"></div>
+      <div className="container">
+
+      </div>
     </>
   );
 }
