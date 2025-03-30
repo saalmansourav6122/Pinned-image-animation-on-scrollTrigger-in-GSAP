b4ea73aee174774414a21d4b742a4a922ca7d726
saalmansourav6122 <saalmansourav111@gmail.com>
2025-03-30 17:11:28 +0600

    added

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,35 @@ import "./App.css";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap/all";
 function App() {
+  const headLine = "We can do for You";
   const array = [
     {
-      headLine: "We can do for You",
       info: [
         {
           title: "One",
-          description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet commodi vel velit libero non deserunt veniam dolores quae cupiditate tenetur?"
+          description:
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet commodi vel velit libero non deserunt veniam dolores quae cupiditate tenetur?",
         },
         {
           title: "Two",
-          description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet commodi vel velit libero non deserunt veniam dolores quae cupiditate tenetur?"
+          description:
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet commodi vel velit libero non deserunt veniam dolores quae cupiditate tenetur?",
         },
         {
           title: "Three",
-          description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet commodi vel velit libero non deserunt veniam dolores quae cupiditate tenetur?"
-        }
+          description:
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet commodi vel velit libero non deserunt veniam dolores quae cupiditate tenetur?",
+        },
       ],
-      image: ["https://picsum.photos/200/300", "https://picsum.photos/id/237/200/300", "https://picsum.photos/seed/picsum/200/300", "https://picsum.photos/200/300?grayscale"]
-    }
+      image: [
+        "https://picsum.photos/200/300",
+        "https://picsum.photos/id/237/200/300",
+        "https://picsum.photos/seed/picsum/200/300",
+        "https://picsum.photos/200/300?grayscale",
+      ],
+    },
   ];
-  
+  const infoArray = array[0].info;
   useGSAP(() => {
     gsap.to(".box", {
       duration: 1,
@@ -33,7 +41,21 @@ function App() {
   return (
     <>
       <div className="container">
-
+        <h1 className=" text-2xl font-bold text-center my-4">{headLine}</h1>
+        <div className="text_image">
+          <div className="left">
+            {infoArray.forEach((item) => {
+              console.log("Title:", item.title);
+              console.log("Description:", item.description);
+            })}
+            <div></div>
+          </div>
+          <div className="right">
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
       </div>
     </>
   );
