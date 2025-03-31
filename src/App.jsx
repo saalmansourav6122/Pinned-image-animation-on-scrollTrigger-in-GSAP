bc0112a083bf10b04f63d0752feff17cb9c4dd19
saalmansourav6122 <saalmansourav111@gmail.com>
2025-03-31 22:31:56 +0600

    committed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import "./App.css";
 import { useGSAP } from "@gsap/react";
-import gsap from "gsap/all";
+import gsap, { ScrollTrigger } from "gsap/all";
+gsap.registerPlugin(ScrollTrigger);
 function App() {
   const headLine = "We can do for You";
   const array = [
@@ -27,33 +28,43 @@ function App() {
         "https://picsum.photos/200/300",
         "https://picsum.photos/id/237/200/300",
         "https://picsum.photos/seed/picsum/200/300",
-        "https://picsum.photos/200/300?grayscale",
+        
       ],
     },
   ];
   const infoArray = array[0].info;
+  const infoImage = array[0].image;
   useGSAP(() => {
-    gsap.to(".box", {
-      duration: 1,
-      rotate: 360,
-    });
+    gsap.set
+      ScrollTrigger.create({
+        trigger: ".right",
+        markers: true,
+        start: "top top",
+        end: "bottom bottom",
+        scrub: 1,
+        
+      })
+
   });
   return (
     <>
       <div className="container">
         <h1 className=" text-2xl font-bold text-center my-4">{headLine}</h1>
-        <div className="text_image">
-          <div className="left">
-            {infoArray.forEach((item) => {
-              console.log("Title:", item.title);
-              console.log("Description:", item.description);
-            })}
-            <div></div>
+        <div className="text_image flex justify-evenly gap-2.5">
+          <div className="left w-[40%]">
+            {infoArray.map((item,id) => (
+              <div key={id} className="h-screen flex flex-col justify-center">
+                <h3 className=" font-bold text-center">{item.title}</h3>
+                <p>{ item.description}</p>
+              </div>
+            ))}
           </div>
           <div className="right">
-            <div></div>
-            <div></div>
-            <div></div>
+            {infoImage.map((item, id) => (
+              <div key={id} id="pin_box" className="relative border w-[400px] border-amber-300 h-screen flex flex-col justify-center">
+                <img className=" absolute" src={item} alt="" />
+              </div>
+            ))}
           </div>
         </div>
       </div>
